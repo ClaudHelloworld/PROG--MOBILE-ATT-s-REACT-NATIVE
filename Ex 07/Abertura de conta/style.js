0783e3fe4cb3ee6ebeed5c61f0c979fb4aed66bd
Claudinei Gomes <60714768+ClaudHelloworld@users.noreply.github.com>
2022-09-25 14:26:53 -0300

    v 1.2

---
+++ Ex 07/Abertura de conta/style.js
@@ -0,0 +1,66 @@
+import { StyleSheet } from 'react-native';
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    subcontainer: {
+        flex: 4
+    },
+    titulo: {
+        color: 'purple',
+        fontSize: 40,
+        alignSelf: 'center',
+        margin: 50,
+        flex: 1,
+    },
+    textInput: {
+        alignSelf: 'stretch',
+        padding: 10,
+        marginLeft: 50,
+        borderBottomColor: '#000',
+        margin: 5,
+        marginRight: 50,
+
+        borderBottomColor: '#000', // Add this to specify bottom border color
+        borderBottomWidth: 2     // Add this to specify bottom border thickness
+    },
+    form: {
+        flexDirection: 'row',
+        
+    },
+    button: {
+        backgroundColor: 'purple',
+        width: '20%',
+        borderColor: 'black',
+        borderStyle: 'solid',
+        borderRadius: 10,
+        padding: 10
+    },
+    buttonText: {
+        alignSelf: 'center',
+        fontSize: 20,
+        color: 'white'
+    },
+    switch: {
+        marginLeft: 10,
+    },
+    sliderText:
+    {
+        textAlign: 'center',
+        fontSize: 25,
+        color: 'green'
+    },
+    resultados:
+    {
+        alignContent: 'left',
+        justifyContent: 'left',
+        width: 500,
+    },
+});
+
+export { styles }
