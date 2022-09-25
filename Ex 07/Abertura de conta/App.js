2b0201fb5805e43fd9bf8fc9ed568f79c121d52c
Claudinei Gomes <60714768+ClaudHelloworld@users.noreply.github.com>
2022-09-25 14:27:52 -0300

    v 1.2

--- Ex 07/Abertura de conta/App.js
+++ Ex 07/Abertura de conta/App.js
@@ -1,34 +1,136 @@
-import React from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { StatusBar } from 'expo-status-bar';
+import { Text, TextInput, View, Picker, Switch, Pressable } from 'react-native';
+import Slider from '@react-native-community/slider';
+import React, { Component } from 'react';
+import { styles } from './styles.js';
 
-const UselessTextInput = () => {
-  const [text, onChangeText] = React.useState("Useless Text");
-  const [number, onChangeNumber] = React.useState(null);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      nome: '',
+      idade: 0,
+      sexo: "Não definido",
+      escolaridade: '',
+      brasileiro: false,
+      nacionalidade: ' ',
+      limite: 50.00,
+      resultado: ''
+    }
+    this.mostrarDados = this.mostrarDados.bind(this);
+    this.eBrasileiro = this.eBrasileiro.bind(this);
+  }
 
-  return (
-    <SafeAreaView>
-      <center><h1>Abertura de conta</h1></center>
-      <TextInput
-        style={styles.input}
-        placeholder="Nome"
-      />
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="Idade"
-      />
-    </SafeAreaView>
-  );
-};
+  mostrarDados() {
+    this.setState({
+      resultado:
+        `Dados informados: \n` +
+        `Nome: ${this.state.nome} \n` +
+        `Idade: ${this.state.idade} \n` +
+        `Sexo: ${this.state.sexo} \n` +
+        `Escolaridade: ${this.state.escolaridade} \n` +
+        `Nacionalidade: ${this.state.nacionalidade} \n` +
+        `Limite: ${this.state.limite}`
+    })
+  }
 
-const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-});
+  eBrasileiro() {
+    if (this.state.brasileiro == true) {
+      this.setState({ nacionalidade: "Brasileiro" })
+    }
+    else if (this.state.brasileiro == false) {
+      this.setState({ nacionalidade: "Não Brasileiro" })
+    }
+  }
 
-export default UselessTextInput;
+  render() {
+    return (
+
+      <View style={styles.container}>
+        <Text style={styles.titulo}>NUBANK</Text>
+        <Text style={styles.titulo}>Abertura de conta</Text>
+      <View style={styles.subcontainer}>
+        <View style={styles.form}>
+          <Text>Nome: </Text>
+          <TextInput placeholder="Nome"
+            style={styles.textInput}
+            onChangeText={(n) => this.setState({ nome: n })}
+          />
+        </View>
+        <View style={styles.form}>
+          <Text>Idade: </Text>
+          <TextInput placeholder="Idade"
+            style={styles.textInput}
+            onChangeText={(i) => this.setState({ idade: i })}
+          />
+          
+        </View>
+      
+        <View style={styles.form}>
+          <Text style={{ justifyContent: 'left' }}>Sexo: </Text>
+          <Picker
+            selectedValue={this.state.sexo}
+            onValueChange={(itemValue) => this.setState({ sexo: itemValue })}
+          >
+            <Picker.Item value={''} label='Não informado...' />
+            <Picker.Item value={"Masculino"} label="Masculino" />
+            <Picker.Item value={"Feminino"} label="Feminino" />
+          </Picker>
+        </View>
+
+        <View style={styles.form}>
+          <Text>Escolaridade: </Text>
+          <Picker
+            onValueChange={(itemValue) => this.setState({ escolaridade: itemValue })}
+          >
+            <Picker.Item value={''} label="Não informado..." />
+            <Picker.Item value={"Ensino Fundamental"} label="Ensino Fundamental" />
+            <Picker.Item value={"Ensino Médio"} label="Ensino Médio" />
+            <Picker.Item value={"Ensino Superior"} label="Ensino Superior" />
+          </Picker>
+        </View>
+
+        <View style={styles.form}>
+          <Text>Limite: </Text>
+          <Slider minimumValue={50.00}
+            maximumValue={1500.00}
+            style={{ width: 200, height: 40 }}
+            onValueChange={(valorSelecionado) => this.setState({ limite: valorSelecionado })}
+            value={this.state.limite}
+            minimumTrackTintColor="#000000"
+            maximumTrackTintColor="#FFFFFF"
+          />
+          <Text style={styles.sliderText}>
+            R$ {this.state.limite.toFixed(0)}
+          </Text>
+        </View>
+
+        <View style={styles.form}>
+          <Text>Brasileiro? </Text>
+          <Text>{this.state.brasileiro ? 'Sim' : 'Não'}</Text>
+          <Switch
+            style={styles.switch}
+            value={this.state.brasileiro}
+            onValueChange={(valor) => this.setState({ brasileiro: valor })}
+          />
+        </View>
+
+        <Text>{this.state.nacionalidade}</Text>
+        <Pressable style={styles.button}>
+          <Text
+            style={styles.buttonText}
+            onPress={this.mostrarDados}
+          >
+            Confirmar
+          </Text>
+        </Pressable>
+
+        <Text style={styles.resultados}>{this.state.resultado}</Text>
+
+        </View>
+      </View>
+    )
+  }
+}
+
+export default App;
